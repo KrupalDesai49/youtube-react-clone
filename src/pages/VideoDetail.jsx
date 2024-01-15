ef61fd72169a37af4345d2e19c3a51fc5374f21d
KrupalDesai49 <98258146+KrupalDesai49@users.noreply.github.com>
2024-01-15 18:28:48 +0530

    vercel bug solving

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -3,13 +3,12 @@ import React from "react";
 import ChannelButtonDetail from "../components/ChannelButtonDetail";
 import Description from "../components/Description";
 import VideoList from "../components/VideoList";
+import Comments from "../components/Comments";
 
 const VideoDetail = ({ videoData, allData, sendVideoData }) => {
-
- 
   return (
     <div class="mx-auto max-w-[85rem] px-6 py-5 font-roboto text-white">
-      <div className="flex flex-col space-x-0 md:flex-row items-center md:items-start md:space-x-6">
+      <div className="flex flex-col items-center space-x-0 md:flex-row md:items-start md:space-x-6">
         {/* Left Video Player Section */}
         {/* Video & Channel Details */}
         <div className="flex flex-col md:w-[70%]">
@@ -29,10 +28,17 @@ const VideoDetail = ({ videoData, allData, sendVideoData }) => {
 
           {/* Channel & Buttons Section */}
 
-          <ChannelButtonDetail sendVideoData={sendVideoData} videoData={videoData} />
+          <ChannelButtonDetail
+            sendVideoData={sendVideoData}
+            videoData={videoData}
+          />
 
           {/* Comment Section */}
           <Description videoData={videoData} />
+
+          {/* Comment Section */}
+          <Comments />
+          
         </div>
 
         {/* Right Video List Section */}
