35b7f42c24aa7e761174519fe9e2c1dad1cf8543
KrupalDesai49 <98258146+KrupalDesai49@users.noreply.github.com>
2024-01-21 19:52:38 +0530

    youtube short autoplay & !mute

--- src/components/YoutubeShort.jsx
+++ src/components/YoutubeShort.jsx
@@ -5,35 +5,42 @@ import { useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 const YoutubeShort = ({ index, videoId }) => {
-  const ref = useRef();
+//   const ref = useRef();
   const [inViewRef, inView] = useInView({
     //    triggerOnce: false,
     // threshold: 0.5,
     rootMargin:'100px'
+   
+  });
+  const [inViewRef2, inView2] = useInView({
+    //    triggerOnce: false,
+    threshold: 1,
+    // rootMargin:'100px'
   });
 
-  //   useEffect(() => {
-  //     if (inView) {
-
-  //     }
-  //   }, [inView, videoId, onEnded]);
+  const setRefs = (node) => {
+    inViewRef2(node);
+    inViewRef(node);
+   };
 
   console.log(index, "::", inView);
+  console.log(index,index, "::", inView2);
 
   return (
     <div>
-      <div className="" key={index}>
+      <div className="" >
         <iframe
           // width="319"
           // height="567"
-          ref={inViewRef}
-          className={true&&"h-[35.5rem] rounded-xl border-2 border-white"}
+        //   ref={inViewRef}
+          ref={setRefs}
+          className="h-[35.5rem] rounded-xl border-2 border-white"
           // src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`}
-          src={inView?`https://www.youtube.com/embed/${videoId}?`:null}
+          src={inView?`https://www.youtube.com/embed/${videoId}?${inView2?'autoplay=1&mute=0':'autoplay=0&mute=1'}`:null}
           title="We Bought The Cheapest iPhone 15 Pro In the World!"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
--- src/pages/VideoComponent.jsx
+++ src/pages/VideoComponent.jsx
@@ -4,7 +4,8 @@ import { useInView } from "react-intersection-observer";
 import YoutubeShort from "../components/YoutubeShort";
 
 const VideoComponent = () => {
-  const videoIds = ["5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA"]; // Replace with your actual video IDs
+  // const videoIds = ["5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA"]; // Replace with your actual video IDs
+  const videoIds = ["5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA","5vFhplWQJis", "WhD28kAD7oE", "tUqGNaEv_AA"]; // Replace with your actual video IDs
   // const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 
   // const handleVideoEnded = () => {
@@ -18,12 +19,14 @@ const VideoComponent = () => {
     <div>
       <div className="flex flex-col items-center space-y-6">
         {videoIds.map((videoId, index) => (
+          <div className="" key={index}>
           <YoutubeShort
             videoId={videoId}
             // onEnded={handleVideoEnded}
             // isActive={index === currentVideoIndex}
             index={index}
           />
+          </div>
         ))}
       </div>
     </div>
