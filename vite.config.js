1c8a8efae3e0df836d44457c2aafecb87b548637
KrupalDesai49 <98258146+KrupalDesai49@users.noreply.github.com>
2024-01-30 22:05:23 +0530

    remove alice

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      // Add more aliases as needed
-    },
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './src'),
+  //     // Add more aliases as needed
+  //   },
+  // }
 })
