1018d9f94cbdbf126439dbeed7ae7e9a31ff67fa
KrupalDesai49 <98258146+KrupalDesai49@users.noreply.github.com>
2024-02-15 16:20:11 +0530

    Description Code Updated

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,11 @@
+import { useAtom } from "jotai";
+import moment from "moment";
 import React, { useState } from "react";
+import { user_data } from "../context/atom";
 
 const Description = ({ videoItem }) => {
   const [desClicked, setDesClicked] = useState(false);
+  const [userData, setUserData] = useAtom(user_data);
 
   const desFunc = () => setDesClicked((e) => !e);
 
@@ -16,24 +20,28 @@ const Description = ({ videoItem }) => {
         {/* Views & Date */}
         <div className="flex">
           <p className="text-sm font-[500]">
-            {desClicked ? videoItem?.detail_view : videoItem?.view}
+            {videoItem?.view.toLocaleString('en-US')}
           </p>
           <p className="pl-1 text-sm font-[500]">views</p>
           <p className="pl-2.5 text-sm font-[500]">
-            {desClicked
-              ? videoItem?.detail_upload_time
-              : videoItem?.upload_time}
+            {!desClicked
+              ? moment(userData.filter(email => email.id ==videoItem?.channel_email)[0]?.timestamp).fromNow()
+              : new Intl.DateTimeFormat('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric'
+              }).format(new Date(userData.filter(email => email.id ==videoItem?.channel_email)[0]?.timestamp))}
           </p>
         </div>
 
-        {/* Desacription */}
-        <p
+        {/* Description */}
+        <pre
           className={`${
             desClicked ? "line-clamp-none" : "line-clamp-2"
-          } pt-1 text-sm`}
+          } pt-1 text-sm  whitespace-pre-wrap font-roboto`}
         >
           {videoItem?.description}
-        </p>
+        </pre>
 
         {/* More  */}
         <p
